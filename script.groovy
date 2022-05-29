045da72d1fd3ddbf3701876f90cc8921f80b2e7f
SyrineDkl <doukali_syrine@yahoo.com>
2022-05-29 21:20:42 +0000

    Update Jenkinsfile, script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t ayadinou/maven-build-repo:2.0 .'
+        sh 'docker build -t ayadinou/maven-build-repo::${IMAGE_NAME}.'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push ayadinou/maven-build-repo:2.0'
+        sh 'docker push ayadinou/maven-build-repo::${IMAGE_NAME}'
     }
 }
 
