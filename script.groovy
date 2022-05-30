22670ab33e71fdc2eb22b10dc93ecf18c9950460
SyrineDkl <doukali_syrine@yahoo.com>
2022-05-30 13:45:34 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,8 +7,8 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'NexusCredentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t ayadinou/maven-build-repo::${IMAGE_NAME}.'
-        sh "echo $PASS | docker login http://20.224.230.246:8083/repository/docker-hosted/"
-        sh 'docker push http://20.224.230.246:8083/repository/docker-hosted/'
+        sh "echo $PASS | docker login 20.224.230.246:8083/repository/docker-hosted/"
+        sh 'docker push 20.224.230.246:8083/repository/docker-hosted/'
     }
 }
 
