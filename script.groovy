c8be4034714092ec74a2be4bfafa38bf00548c59
mohamed ayadi <ayadi.01.mohamed@gmail.com>
2022-05-30 21:39:45 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'NexusCredentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t bookstore:${IMAGE_NAME} --build-arg BUILD_ID="${IMAGE_NAME}" .'
-        sh 'docker login 40.114.225.176:8083/repository/docker-hosted/ '
+        sh 'docker login -u $USER --password-stdin 40.114.225.176:8083/repository/docker-hosted/ '
         sh 'docker push 40.114.225.176:8083/repository/docker-hosted/'
     }
 }
