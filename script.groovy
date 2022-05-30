6db80c6c0fa88fa700dd28606024b40cc39ab5e1
SyrineDkl <doukali_syrine@yahoo.com>
2022-05-30 21:15:29 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,8 +7,8 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'NexusCredentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t bookstore:${IMAGE_NAME} --build-arg BUILD_ID="${IMAGE_NAME}" .'
-        sh 'docker login 20.224.230.246:8083/repository/docker-hosted/ '
-        sh 'docker push 20.224.230.246:8083/repository/docker-hosted/'
+        sh 'docker login 40.114.225.176:8083/repository/docker-hosted/ '
+        sh 'docker push 40.114.225.176:8083/repository/docker-hosted/'
     }
 }
 
