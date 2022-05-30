9f1395ddbc924950d648d45aac0e12466e34f16d
mohamed ayadi <ayadi.01.mohamed@gmail.com>
2022-05-30 22:03:55 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -8,7 +8,7 @@ def buildImage() {
     withCredentials([usernamePassword(credentialsId: 'dockerhubCredentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t bookstore:${IMAGE_NAME} --build-arg BUILD_ID="${IMAGE_NAME}" .'
         sh 'echo $PASS |docker login -u $USER --password-stdin  '
-        sh 'docker push 40.114.225.176:8083/repository/docker-hosted/'
+        sh 'docker push ayadinou/maven-build-repo/bookstore:${IMAGE_NAME}'
     }
 }
 
