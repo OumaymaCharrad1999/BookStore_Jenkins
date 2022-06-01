58d20079d7a445ae0d0d42015f2c662fdd972553
Oumayma_Charrad <charradoumayma1@gmail.com>
2022-06-01 15:09:12 +0100

    Update

--- src/main/java/com/bookstore/controller/BookController.java
+++ src/main/java/com/bookstore/controller/BookController.java
@@ -30,10 +30,10 @@ public class BookController {
 		return new ResponseEntity<List<Book>>(books, HttpStatus.OK);
 	}
 	
-	@GetMapping("books/{id}")
+	/*@GetMapping("books/{id}")
 	public ResponseEntity<Book> getBook(@PathVariable("id") Integer id){
 		Book book = service.getBook(id);
-		return new ResponseEntity<Book>(book, HttpStatus.OK);
+		return new ResponseEntity<Book>(book, HttpStatus.OK);*/
 	}
 	
 	@PostMapping("books")
