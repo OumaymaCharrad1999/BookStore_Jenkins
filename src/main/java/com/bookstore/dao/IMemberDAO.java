ff967116352fe7c8a1a654046afe550605400523
mohamed ayadi <ayadi.01.mohamed@gmail.com>
2022-06-01 12:29:12 +0000

    Update src/main/java/com/bookstore/dao/IMemberDAO.java

---
+++ src/main/java/com/bookstore/dao/IMemberDAO.java
@@ -0,0 +1,15 @@
+package com.bookstore.dao;
+
+import java.util.List;
+
+import com.bookstore.entity.Member;
+
+public interface IMemberDAO {
+	
+	List<Member> getMembers();
+	Member getMember(int bookId);
+	Member createMember(Member member);
+	Member updateMember(int memberId,Member member);
+	boolean deleteMember(int memberId);
+
+}
