352d12c1352e233cdc4cc1808e13590e85b0582a
mohamed ayadi <ayadi.01.mohamed@gmail.com>
2022-06-01 12:27:07 +0000

    Update src/main/java/com/bookstore/entity/Member.java

--- src/main/java/com/bookstore/entity/Member.java
+++ src/main/java/com/bookstore/entity/Member.java
@@ -1,1 +1,75 @@
+package com.bookstore.entity;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="member")
+public class Member implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="cin")
+	private int cin;
+	
+	@Column(name="nom")
+	private String nom;
+	
+	@Column(name="prenom")
+	private String prenom;
+	
+	@Column(name="adresse")
+	private String adresse;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCIN() {
+		return cin;
+	}
+
+	public void setCIN(int cin) {
+		this.cin = cin;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+
+}
