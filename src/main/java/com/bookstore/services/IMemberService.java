00186546f0d83f5ad9294f552ab64feec9021b81
mohamed ayadi <ayadi.01.mohamed@gmail.com>
2022-06-01 12:32:11 +0000

    Update src/main/java/com/bookstore/services/IMemberService.java

---
+++ src/main/java/com/bookstore/services/IMemberService.java
@@ -0,0 +1,15 @@
+package com.bookstore.services;
+
+import java.util.List;
+
+import com.bookstore.entity.Member;
+
+public interface IMemberService {
+	
+	List<Member> getMembers();
+	Member getMember(int memberId);
+	Member createMember(Member member);
+	Member updateMember(int memberId, Member member);
+	boolean deleteMember(int memberId);
+
+}
